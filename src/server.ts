0859adb605873627d0e0071d4a2c76313e25f69e
Rony <rony@Ronys-Air.lan>
2024-04-23 19:33:38 +0600

    server error handaller

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,29 @@
 import { Server } from 'http';
 import app from './app';
+
 const port = 3000;
 
 async function main() {
   const server: Server = app.listen(port, () => {
     console.log('App Listing port on ', port);
+  });
+
+  const existHandaller = () => {
+    if (server) {
+      server.close(() => {
+        console.info('Server is Closed !!');
+      });
+    }
+    process.exit(1);
+  };
+
+  process.on('uncaughtException', (error) => {
+    console.log(error);
+    existHandaller();
+  });
+  process.on('unhandledRejection', (error) => {
+    console.log(error);
+    existHandaller();
   });
 }
 
